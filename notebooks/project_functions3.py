b182d4d16cd92e331d4a0def86a216530a555144
OneInA1E6 <andrew.dunn213@gmail.com>
2022-11-26 19:47:48 -0800

    mess with method chaining

--- notebooks/project_functions3.py
+++ notebooks/project_functions3.py
@@ -5,16 +5,25 @@ import seaborn as sns
 
 def load_and_process(path) :
     dataLength = 9357
-    data = pd.read_csv(path, sep=';', decimal=',', nrows=dataLength).dropna(axis=1, how='all')
+    data = (
+        pd.read_csv(path, sep=';', decimal=',', nrows=dataLength)
+        .dropna(axis=1, how='all')
+        .replace(-200,np.nan)
+    )
     return data
 
 def createNonMetaldf(df) :
-    nonMetal = pd.DataFrame(df[['Date', 'CO(GT)', 'C6H6(GT)', 'NO2(GT)']])
-    nonMetal = nonMetal.replace(-200, np.nan)
+    nonMetal = (
+        pd.DataFrame(df[['Date', 'CO(GT)', 'C6H6(GT)', 'NO2(GT)']])
+        .replace(-200, np.nan)
+        
+    )
     return nonMetal
 
 def getMeans(df) :
-    m = [3]
+    m = [None] * 3
     m[0] = df['C6H6(GT)'].mean()
+    print(m[0])
     m[1] = df['NO2(GT)'].mean()
     m[2] = df['CO(GT)'].mean()
+    return m
