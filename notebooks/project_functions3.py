74a68031be726c64a48fed5c2a7d67ebe3e26a0f
OneInA1E6 <andrew.dunn213@gmail.com>
2022-12-02 20:44:13 -0800

    Analysis 3 complete

--- notebooks/project_functions3.py
+++ notebooks/project_functions3.py
@@ -9,15 +9,28 @@ def load_and_process(path) :
         pd.read_csv(path, sep=';', decimal=',', nrows=dataLength)
         .dropna(axis=1, how='all')
         .replace(-200,np.nan)
+        
     )
+    data['Time'] = data['Time'].astype(str).apply(lambda x: x[0:2]).astype(int)
     return data
 
 def createNonMetaldf(df) :
-    nonMetal = (
-        pd.DataFrame(df[['Date', 'CO(GT)', 'C6H6(GT)', 'NO2(GT)']])
-        .replace(-200, np.nan)
-        
+    benzConst = 1597
+    noConst = 327
+    coConst = 20
+    df1 = (
+        pd.DataFrame(df[['CO(GT)', 'C6H6(GT)', 'NO2(GT)']])
+        .div({'CO(GT)':(coConst), 'C6H6(GT)':(benzConst), 'NO2(GT)':(noConst)})
     )
+    
+    nonMetal = (
+        pd.DataFrame(df[['Date']])
+        .merge(df1, left_index=True, right_index=True)
+        .replace(-200, np.nan)
+        #.melt(id_vars=['Date'], value_vars=['CO(GT)', 'C6H6(GT)', 'NO2(GT)'])   
+    )
+    
+    
     return nonMetal
 
 def getMeans(df) :
@@ -27,3 +40,11 @@ def getMeans(df) :
     m[1] = df['NO2(GT)'].mean()
     m[2] = df['CO(GT)'].mean()
     return m
+
+# Get days that are immediately dangerous for NO2 concentration
+def getImmDays(df) :
+    filtered = (
+        pd.DataFrame(df.loc[(df['Date'] == '03/02/2005') | (df['Date'] == '11/02/2005')])
+        
+    )
+    return filtered
